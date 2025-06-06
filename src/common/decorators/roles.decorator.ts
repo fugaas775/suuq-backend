4b06957e654e3c94b6efdbea253acb3f10e4144f
fugaas775 <127171617+fugaas775@users.noreply.github.com>
2025-06-06 16:27:55 +0300

    Update roles.decorator.ts

--- src/common/decorators/roles.decorator.ts
+++ src/common/decorators/roles.decorator.ts
@@ -1,5 +1,6 @@
 import { SetMetadata } from '@nestjs/common';
+import { UserRole } from '../../auth/roles.enum'; // Adjust path if needed
 
 export const ROLES_KEY = 'roles';
 
-export const Roles = (...roles: string[]) => SetMetadata(ROLES_KEY, roles);
+export const Roles = (...roles: UserRole[]) => SetMetadata(ROLES_KEY, roles);
