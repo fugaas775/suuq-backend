485889ee616a254b7cffab30d547db688390b59c
Fugaas775 <fugaas775@gmail.com>
2025-06-07 08:14:11 +0000

    Update July 6 Morning

--- src/media/dto/bulk-media.dto.ts
+++ src/media/dto/bulk-media.dto.ts
@@ -3,7 +3,7 @@ import { CreateMediaDto } from './create-media.dto';
 
 export class BulkMediaDto {
   @ApiProperty({ type: [CreateMediaDto] })
-  media: CreateMediaDto[];
+  media!: CreateMediaDto[];
 
   constructor(partial?: Partial<BulkMediaDto>) {
     this.media = partial?.media?.map((m) => new CreateMediaDto(m)) ?? [];
